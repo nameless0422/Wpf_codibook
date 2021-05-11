6ac9963ea5633f3032f6c390503ef4c8cdce5566
nameless0422 <cjy042218@gmail.com>
2021-05-12 01:37:37 +0900

    PopUp 메뉴 추가

---
+++ MVVM/View/PopUp/SettingPopUp.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace codibook.MVVM.View.PopUp
+{
+    /// <summary>
+    /// SettingPopUp.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SettingPopUp : Window
+    {
+        public SettingPopUp()
+        {
+            InitializeComponent();
+        }
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // 타이틀바 드래그
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
---
+++ MVVM/View/PopUp/UserPopUp.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace codibook.MVVM.View.PopUp
+{
+    /// <summary>
+    /// UserPopUp.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class UserPopUp : Window
+    {
+        public UserPopUp()
+        {
+            InitializeComponent();
+        }
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        // 타이틀바 드래그
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+    }
+}
