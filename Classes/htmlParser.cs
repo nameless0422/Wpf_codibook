60f18188b2a86baf4340f4bc85402d97fc7d1336
nameless0422 <cjy042218@gmail.com>
2021-05-25 20:45:29 +0900

    GetOgTitle, GetOgImage 메소드를 포함하는 htmlParser 클래스 생성

---
+++ Classes/htmlParser.cs
@@ -0,0 +1,32 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codibook.Classes
+{
+    public class htmlParser
+    {
+        public string GetOgTitle(string link)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument htmlDoc = web.Load(link);
+            HtmlNodeCollection ogTitle = htmlDoc.DocumentNode.SelectNodes("//meta[@property='og:title']");
+
+            return ogTitle[0].Attributes["content"].Value;
+        }
+
+        public string GetOgImage(string link)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument htmlDoc = web.Load(link);
+
+            HtmlNodeCollection ogImage = htmlDoc.DocumentNode.SelectNodes("//meta[@property='og:image']");
+
+            return ogImage[0].Attributes["content"].Value;
+
+        }
+    }
+}
