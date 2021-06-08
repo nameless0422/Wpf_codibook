733b90f92ddd91da89cc47a92bf5e65525941679
darami <darami629@gmail.com>
2021-06-08 21:00:23 +0900

    settingPopUp 및 관련 ViewModel, Command 생성

--- MVVM/ViewModel/Commands/mainCommands/SettingCommand.cs
+++ MVVM/ViewModel/Commands/mainCommands/SettingCommand.cs
@@ -1,6 +1,7 @@
 ﻿using codibook.MVVM.View.PopUp;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,9 +31,68 @@ namespace codibook.MVVM.ViewModel.Commands.mainCommands
         public void Execute(object parameter)
         {
             MainViewModel mainViewModel = parameter as MainViewModel;
+
+            LoadConfiguration();
+            if (settingCityValue != string.Empty)
+            {
+
+            }
+
             SettingPopUp settingPopUp = new SettingPopUp(mainViewModel);
             settingPopUp.Show();
             mainViewModel.SettingCheck = true;
+        }
+
+        // using configurationManager
+        public string settingCityValue = string.Empty;
+
+        public void SaveConfiguration()
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var appSettings = configFile.AppSettings.Settings;
+
+                if (appSettings["PropertyName"] == null)
+                {
+                    appSettings.Add("PropertyName", settingCityValue);
+                }
+
+                else
+                {
+                    appSettings.Remove("PropertyName");
+                    appSettings.Add("PropertyName", settingCityValue);
+                }
+
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("configuration error exception occur!");
+            }
+        }
+
+        public void LoadConfiguration()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result;
+
+                result = appSettings["PropertyName"] ?? string.Empty;
+
+                if (result != string.Empty)
+                {
+                    settingCityValue = result;
+                }
+            }
+
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("configuration error exception occur!");
+            }
         }
     }
 }
