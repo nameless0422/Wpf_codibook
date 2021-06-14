64ed35be279ae84bb26c58e82d42c471eb20c614
Lee.s.c <l3621841981@gmail.com>
2021-06-14 21:09:30 +0900

    UserPopUp 일부

--- MVVM/ViewModel/Commands/mainCommands/searchCommand.cs
+++ MVVM/ViewModel/Commands/mainCommands/searchCommand.cs
@@ -1,5 +1,7 @@
 ﻿using codibook.MVVM.View;
+using codibook.MVVM.View.PopUp;
 using codibook.MVVM.View.SearchViewListPages;
+using codibook.MVVM.ViewModel.PopUpViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +19,9 @@ namespace codibook.MVVM.ViewModel.Commands.mainCommands
         {
             MainWindow window = parameter as MainWindow;
             return window.Search_Box.Text.Length >= 3;
+
+            UserPopUp userpopup = parameter as UserPopUp;
+            return userpopup.Search_User.Text.Length >= 1;
         }
 
         public void Execute(object parameter)
@@ -25,13 +30,13 @@ namespace codibook.MVVM.ViewModel.Commands.mainCommands
             MainViewModel vm = window.Resources["MainVM"] as MainViewModel;
             SearchViewPage searchPage = new SearchViewPage();
             SearchItemPage searchItemPage = new SearchItemPage();
+            UserPopUp userpopup = parameter as UserPopUp;
             searchPage.DataContext = vm;
             searchItemPage.DataContext = vm.itemVM;
             vm.itemVM.setItemlist(vm.Search_Text,0);
             searchItemPage.itemListView.ItemsSource = vm.itemVM.items;
             searchPage.listframe.Navigate(searchItemPage);
             window.Mainframe.Navigate(searchPage);
-            
         }
     }
 }
--- MVVM/ViewModel/PopUpViewModel/UserPopUpViewModel.cs
+++ MVVM/ViewModel/PopUpViewModel/UserPopUpViewModel.cs
@@ -1,18 +1,57 @@
-﻿using codibook.MVVM.ViewModel.Commands.closeCommands;
+﻿using codibook.MVVM.Model;
+using codibook.MVVM.ViewModel.Commands.closeCommands;
+using codibook.MVVM.ViewModel.Commands.mainCommands;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace codibook.MVVM.ViewModel.PopUpViewModel
 {
-    public class UserPopUpViewModel
+    public class UserPopUpViewModel : INotifyPropertyChanged
     {
+        private string search_user;
+        public string Search_User
+        {
+            get { return search_user; }
+            set
+            {
+                search_user = value;
+                OnPropertyChanged("Search_User");
+            }
+        }
+        public string User_Inrto { get; set; }
+
         public UserPopUpCloseCommand closeCommand { get; set; }
+        public searchCommand SearchCommandProperty { get; set; }
+        public UserPopUpViewModel userVM { get; set; }
+
         public UserPopUpViewModel()
         {
             closeCommand = new UserPopUpCloseCommand();
+            SearchCommandProperty = new searchCommand();
+        }
+
+        public void userNamelist()
+        {
+           
+        }
+
+        public void userNamelist(string username, string userintro)
+        {
+           
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
