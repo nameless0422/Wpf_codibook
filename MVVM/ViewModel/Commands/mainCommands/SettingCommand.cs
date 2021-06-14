547d03289648731fce9543e7bb2c116ea8c41f3c
nameless0422 <cjy042218@gmail.com>
2021-06-15 03:55:08 +0900

    Revert "settingPopUp 기능 구현(인데 대체 왜 적용이 안되는지 모르는...)"
    This reverts commit ee82196cf28250099cb385e6ff50495ae89c7f77.

--- MVVM/ViewModel/Commands/mainCommands/SettingCommand.cs
+++ MVVM/ViewModel/Commands/mainCommands/SettingCommand.cs
@@ -12,6 +12,20 @@ namespace codibook.MVVM.ViewModel.Commands.mainCommands
 {
     public class SettingCommand : ICommand
     {
+        string configCityName;
+        public string ConfigCityName
+        {
+            get
+            {
+                return configCityName;
+            }
+
+            set
+            {
+                configCityName = value;
+            }
+        }
+
         public event EventHandler CanExecuteChanged;
 
         public bool CanExecute(object parameter)
@@ -31,9 +45,77 @@ namespace codibook.MVVM.ViewModel.Commands.mainCommands
         public void Execute(object parameter)
         {
             MainViewModel mainViewModel = parameter as MainViewModel;
+
+            LoadConfiguration();
+            if (settingCityValue != string.Empty)
+            {
+                configCityName = settingCityValue;
+            }
+
+            else
+            {
+                WeatherAPI weatherAPI = new WeatherAPI();
+                settingCityValue = weatherAPI.cityname;
+                SaveConfiguration();
+
+                configCityName = settingCityValue;
+            }
+
             SettingPopUp settingPopUp = new SettingPopUp(mainViewModel);
             settingPopUp.Show();
             mainViewModel.SettingCheck = true;
+        }
+
+        // using configurationManager
+        public string settingCityValue = string.Empty;
+
+        public void SaveConfiguration()
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var appSettings = configFile.AppSettings.Settings;
+
+                if (appSettings["PropertyName"] == null)
+                {
+                    appSettings.Add("PropertyName", settingCityValue);
+                }
+
+                else
+                {
+                    appSettings.Remove("PropertyName");
+                    appSettings.Add("PropertyName", settingCityValue);
+                }
+
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("configuration error exception occur!");
+            }
+        }
+
+        public void LoadConfiguration()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result;
+
+                result = appSettings["PropertyName"] ?? string.Empty;
+
+                if (result != string.Empty)
+                {
+                    settingCityValue = result;
+                }
+            }
+
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("configuration error exception occur!");
+            }
         }
     }
 }
